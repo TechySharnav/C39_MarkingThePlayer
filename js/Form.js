463b256462f0a2c29cd41f084aa4b2131e94df19
TechySharnav <sharnav123@gmail.com>
2020-06-29 19:46:33 +0530

    Added Reset Button and Marked the active player

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,7 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset = createButton('RESET');
   }
   hide(){
     this.greeting.hide();
@@ -19,8 +20,8 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-2
-    this.button.mousePressed(()=>{
+
+      this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
       player.name = this.input.value();
@@ -30,6 +31,12 @@ class Form {
       player.updateCount(playerCount);
       this.greeting.html("Hello " + player.name)
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+
+    this.reset.position(displayWidth-100, 20);
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
     });
 
   }
--- js/Game.js
+++ js/Game.js
@@ -30,33 +30,39 @@ class Game {
     }
 
     car1 = createSprite(100,200);
-    car1.addImage("car1",car1_img);
+    car1.addImage("car1Image", car1Img);
+
     car2 = createSprite(300,200);
-    car2.addImage("car2",car2_img);
+    car2.addImage("car2Image", car2Img);
+
     car3 = createSprite(500,200);
-    car3.addImage("car3",car3_img);
+    car3.addImage("car3Image", car3Img);
+
     car4 = createSprite(700,200);
-    car4.addImage("car4",car4_img);
+    car4.addImage("car4Image", car4Img);
+
     cars = [car1, car2, car3, car4];
   }
 
   play(){
     form.hide();
-    
+
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+
       background(rgb(198,135,103));
-      image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
-      
+
+      image(track, 0,-displayHeight*4 ,displayWidth ,displayHeight*5);
+
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
-      var y;
+      var x = 450;
+      var y = 0;
 
       for(var plr in allPlayers){
         //add 1 to the index for every loop
@@ -66,34 +72,49 @@ class Game {
         x = x + 200;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
-        cars[index-1].y = y;
+
+        //console.log(cars[index-1]);
+
+         cars[index-1].x = x;
+         cars[index-1].y = y;
 
         if (index === player.index){
+          strokeWeight(10);
+          stroke('red');
+          ellipse(x,y,60,60);
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y;
+          camera.position.y = cars[index-1].y
         }
+
+        //console.log(index-1);
+        //console.log(index, player.index);
        
         //textSize(15);
         //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
 
+      
+
     }
 
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=10
-      player.update();
+    if(gameState === 1){
+      if(keyIsDown(UP_ARROW) && player.index !== null){
+        player.distance +=10; 
+        player.update();
     }
+  }
 
-    if(player.distance > 3860){
+    if(player.distance > 3000){
       gameState = 2;
     }
-   
+
     drawSprites();
   }
 
+
   end(){
-    console.log("Game Ended");
+    console.log("Game ENDED");
+    game.update(2);
   }
 }
